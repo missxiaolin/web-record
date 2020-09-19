ac71e3e540fd2041d43afa943e55cb9555b55687
xiaolin <462441355@qq.com>
2020-09-19 23:00:30 +0800

    * 引入饿了吗

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,20 @@
   </div>
 </template>
 
-<style lang="scss">
-html, body {
-  margin: 0;
-  padding: 0;
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+body, html {
   width: 100%;
   height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
