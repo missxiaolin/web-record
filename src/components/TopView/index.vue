671765c427257b5921e842fb906625a714aefeae
xiaolin <462441355@qq.com>
2020-09-20 16:33:02 +0800

    * top 样式

--- src/components/TopView/index.vue
+++ src/components/TopView/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>topview</div>
+  <div class="top-view">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮显示
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
