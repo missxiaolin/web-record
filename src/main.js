d9e0d3fc7dfc94948474970cdb3d8f5518c18c4b
xiaolin <462441355@qq.com>
2020-09-26 21:29:45 +0800

    * 测试

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import Echarts from 'echarts'
+import VueEcharts from 'vue-echarts'
 
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = Echarts
+Vue.component('v-chart', VueEcharts)
 
 new Vue({
   router,
