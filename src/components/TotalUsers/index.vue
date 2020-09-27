41a6b8bff65bcce2adef66eadc9b437f1f6ca077
xiaolin <462441355@qq.com>
2020-09-27 09:38:07 +0800

    * fix

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -1,107 +1,122 @@
 <template>
   <div>
-      <CommonCard title="累积用户数" value="81,014">
-        <template>
-          <v-chart :options="getOptions()" />
-        </template>
-        <template v-slot:footer>
-          <div class="total-users-footer">
-            <span>日同比 </span>
-            <span class="emphasis">8.73%</span>
-            <div class="increase"></div>
-            <span class="month">月同比 </span>
-            <span class="emphasis">3.73%</span>
-            <div class="decrease"></div>
-          </div>
-        </template>
-      </CommonCard>
+    <CommonCard title="累积用户数" value="81,014">
+      <template>
+        <v-chart :options="getOptions()" />
+      </template>
+      <template v-slot:footer>
+        <div class="total-users-footer">
+          <span>日同比 </span>
+          <span class="emphasis">8.73%</span>
+          <div class="increase"></div>
+          <span class="month">月同比 </span>
+          <span class="emphasis">3.73%</span>
+          <div class="decrease"></div>
+        </div>
+      </template>
+    </CommonCard>
   </div>
 </template>
 
 <script>
-import commonCardMixin from '../../mixins/commonCardMixin'
+import commonCardMixin from "../../mixins/commonCardMixin";
 export default {
-    name: 'total-users',
-    mixins: [commonCardMixin],
-    mounted() {
-      const charDom = document.getElementById('total-users-chart')
-      const chart = this.$echarts.init(charDom)
-      chart.setOption({
+  name: "total-users",
+  mixins: [commonCardMixin],
+  mounted() {
+    
+  },
+  methods: {
+    getOptions() {
+      return {
         xAxis: {
-          type: 'value',
+          type: "value",
           show: false, // 是否显示x轴
         },
         yAxis: {
-          type: 'category',
-          show: false
+          type: "category",
+          show: false,
         },
-        series: [{
-          type: "bar",
-          stack: '总量', // 合并
-          data: [200],
-          barWidth: 10,
-          itemStyle: { // 颜色 
-            color: '#45c946'
-          }
-        }, {
-          type: "bar",
-          stack: '总量',
-          data: [270],
-          barWidth: 10,
-          itemStyle: {
-            color: '#eee'
-          }
-        }, { // 自定义图形
-          type: 'custom',
-          data: [200],
-          stack: '总量',
-          renderItem: (params, api) => {
-            const value = api.value(0) // 获取一号元素
-            const endPoint = api.coord([value, 0])
+        series: [
+          {
+            type: "bar",
+            stack: "总量", // 合并
+            data: [200],
+            barWidth: 10,
+            itemStyle: {
+              // 颜色
+              color: "#45c946",
+            },
+          },
+          {
+            type: "bar",
+            stack: "总量",
+            data: [270],
+            barWidth: 10,
+            itemStyle: {
+              color: "#eee",
+            },
+          },
+          {
+            // 自定义图形
+            type: "custom",
+            data: [200],
+            stack: "总量",
+            renderItem: (params, api) => {
+              const value = api.value(0); // 获取一号元素
+              const endPoint = api.coord([value, 0]);
 
-            return {
-              type: 'group', // 线段
-              position: endPoint, // 绘图坐标系
-              children: [{
-                type: 'path',
-                shape: { // 形状
-                  d: 'M273.6 593l212.9-212.9c14.1-14.1 36.9-14.1 50.9 0l213 212.9c22.7 22.7 6.6 61.5-25.5 61.5H299.1c-32.1 0-48.1-38.8-25.5-61.5z',
-                  x: -10,
-                  y: 3,
-                  width: 20,
-                  height: 20,
-                  // layout: 'cover', // 填充
-                },
-                style: {
-                  fill: '#45c946'
-                }
-              }, {
-                type: 'path',
-                shape: { // 形状
-                  d: 'M273.6 431l212.9 212.9c14.1 14.1 36.9 14.1 50.9 0l213-212.9c22.7-22.7 6.6-61.5-25.5-61.5H299.1c-32.1 0-48.1 38.8-25.5 61.5z',
-                  x: -10,
-                  y: -23,
-                  width: 20,
-                  height: 20,
-                  // layout: 'cover'
-                },
-                style: {
-                  fill: '#45c946'
-                }
-              }],
-              
-            }
-          }
-        }],
+              return {
+                type: "group", // 线段
+                position: endPoint, // 绘图坐标系
+                children: [
+                  {
+                    type: "path",
+                    shape: {
+                      // 形状
+                      d:
+                        "M273.6 593l212.9-212.9c14.1-14.1 36.9-14.1 50.9 0l213 212.9c22.7 22.7 6.6 61.5-25.5 61.5H299.1c-32.1 0-48.1-38.8-25.5-61.5z",
+                      x: -10,
+                      y: 3,
+                      width: 20,
+                      height: 20,
+                      // layout: 'cover', // 填充
+                    },
+                    style: {
+                      fill: "#45c946",
+                    },
+                  },
+                  {
+                    type: "path",
+                    shape: {
+                      // 形状
+                      d:
+                        "M273.6 431l212.9 212.9c14.1 14.1 36.9 14.1 50.9 0l213-212.9c22.7-22.7 6.6-61.5-25.5-61.5H299.1c-32.1 0-48.1 38.8-25.5 61.5z",
+                      x: -10,
+                      y: -23,
+                      width: 20,
+                      height: 20,
+                      // layout: 'cover'
+                    },
+                    style: {
+                      fill: "#45c946",
+                    },
+                  },
+                ],
+              };
+            },
+          },
+        ],
         // 设置图片间距
         grid: {
           top: 0,
           bottom: 0,
           left: 0,
-          right: 0
-        }
-      })
-    }
+          right: 0,
+        },
+      };
+    },
+  },
 };
 </script>
 
