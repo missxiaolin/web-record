1e960542b826c4358e3a238e55f9c7953d198624
xiaolin <462441355@qq.com>
2020-09-28 22:46:04 +0800

    * 格式化代码

--- src/components/TodayUsers/index.vue
+++ src/components/TodayUsers/index.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
-      <CommonCard title="进入交易用户数" value="81,014">
-        <template>
-          <v-chart :options="getOptions()" />
-        </template>
-        <template v-slot:footer>
-          <span>退货率 </span>
-          <span class="emphasis">5%</span>
-        </template>
-      </CommonCard>
+    <CommonCard title="进入交易用户数" value="81,014">
+      <template>
+        <v-chart :options="getOptions()" />
+      </template>
+      <template v-slot:footer>
+        <span>退货率 </span>
+        <span class="emphasis">5%</span>
+      </template>
+    </CommonCard>
   </div>
 </template>
 
 <script>
-import commonCardMixin from '../../mixins/commonCardMixin'
+import commonCardMixin from "../../mixins/commonCardMixin";
 export default {
-    name: 'today-users',
-    mixins: [commonCardMixin],
-    mounted() {
-      
-    },
-    methods: {
-      getOptions() {
-        return {
-        color: ['#3398DB'],
+  name: "today-users",
+  mixins: [commonCardMixin],
+  mounted() {},
+  methods: {
+    getOptions() {
+      return {
+        color: ["#3398DB"],
         xAxis: {
-          type: 'category',
-          data: ['00:00', '00:00', '00:00', '00:00', '00:00', '00:00', '00:00', '00:00', '00:00', '11:00', '12:00'],
+          type: "category",
+          data: [
+            "00:00",
+            "00:00",
+            "00:00",
+            "00:00",
+            "00:00",
+            "00:00",
+            "00:00",
+            "00:00",
+            "00:00",
+            "11:00",
+            "12:00",
+          ],
           show: false, // 是否显示x轴
-          barWidth: '60%'
+          barWidth: "60%",
         },
         yAxis: {
           show: false, // 是否显示y轴
@@ -37,15 +47,17 @@ export default {
           top: 0,
           bottom: 0,
           left: 0,
-          right: 0
+          right: 0,
         },
-        series: [{
-          type: 'bar',
-          data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 134]
-        }]
-      }
-      }
-    }
+        series: [
+          {
+            type: "bar",
+            data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 134],
+          },
+        ],
+      };
+    },
+  },
 };
 </script>
 
