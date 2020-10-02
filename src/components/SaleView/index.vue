558a43a10190f6b642099027ef50d3eb5167c5c6
xiaolin <462441355@qq.com>
2020-10-02 11:01:49 +0800

    * 初始化

--- src/components/SaleView/index.vue
+++ src/components/SaleView/index.vue
@@ -1,27 +1,76 @@
 <template>
-  <ve-line :data="data" />
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu 
+          mode="horizontal"
+          :default-active="activeIndex"
+          @select="onMenuSelect"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="redioSelect" size="small">
+              <el-radio-button label="今日" />
+              <el-radio-button label="本周" />
+              <el-radio-button label="本月" />
+              <el-radio-button label="今年" />
+            </el-radio-group>
+            <el-date-picker 
+            type="daterange"
+            v-model="data"
+            range-separator="至"
+            start-placeholde="开始日期"
+            end-placeholde="结束日期"
+            size="small"
+            :picker-options="pickerOptions"
+             />
+          </div>
+        </div>
+      </template>
+      <template>
+        <div>22</div>
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      data: {
-        columns: ['日期', '销售额'],
-        rows: [
-          { '日期': '1月1日', '销售额': 131 },
-          { '日期': '1月12日', '销售额': 500 },
-          { '日期': '1月13日', '销售额': 50 }
-        ]
+      activeIndex: '1',
+      redioSelect: '今日',
+      data: null,
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周 ',
+          onClick(picker) {}
+        }, {
+          text: '最近一个月 ',
+          onClick(picker) {}
+        }, {
+          text: '最近三个月 ',
+          onClick(picker) {}
+        }]
       }
+    }
+  },
+  methods: {
+    onMenuSelect(index) {
+      this.activeIndex = index
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.echarts {
-  width: 100%;
-  height: 100 vh;
+.sales-view {
+  margin-top: 20px;
+  .menu-wrapper {
+    display: flex;
+  }
 }
 </style>
