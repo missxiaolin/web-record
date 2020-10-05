e665c57b57fb3cae802bfac472f6f106dae7206c
xiaolin <462441355@qq.com>
2020-10-05 22:31:52 +0800

    * 百度测试

--- src/components/MapView/index.vue
+++ src/components/MapView/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>895OeoRGBBvHCbZT0kl6hdGn6MvREpbS</div>
+  <div id="container"></div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    var map = new BMap.Map("container"); 
+    var point = new BMap.Point(116.404, 39.915); 
+    map.centerAndZoom(point, 15);  
+  }
+};
 </script>
 
 <style lang="scss">
+#container {
+  width: 100%;
+  height: 500px;
+}
 </style>
