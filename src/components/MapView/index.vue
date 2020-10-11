b642fe43efabf21695fef1b95c1b998d003271b1
xiaolin <462441355@qq.com>
2020-10-11 21:43:37 +0800

    * 初始化

--- src/components/MapView/index.vue
+++ src/components/MapView/index.vue
@@ -1,11 +1,36 @@
 <template>
-  <div id="container">
-    
+  <div class="map-view">
+    <div class="left">
+      <BMapScatter />
+    </div>
+    <div class="right">
+      <el-card shadow="hover">
+        <template v-slot:header>
+          <div class="title">用户月同比增长</div>
+        </template>
+        <template>
+          <div class="chart-wrapper">111</div>
+        </template>
+      </el-card>
+      <el-card shadow="hover">
+        <template v-slot:header>
+          <div class="title">热门搜索</div>
+        </template>
+        <template>
+          <div class="chart-wrapper">222</div>
+        </template>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
+import BMapScatter from '../BMapScatter'
+
 export default {
+  components: {
+    BMapScatter
+  },
   mounted() {
     
   }
@@ -13,8 +38,37 @@ export default {
 </script>
 
 <style lang="scss">
-#container {
+.map-view {
   width: 100%;
-  height: 500px;
+  height: auto;
+  overflow: hidden;
+  display: flex;
+  margin-top: 20px;
+  .left {
+    flex: 0 0 80%;
+    width: 80%;
+    height: 600px;
+  }
+  .right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 20px;
+    .title {
+      display: flex;
+      align-items: center;
+      height: 60px;
+      box-sizing: border-box;
+      border-bottom: 1px solid #eee;
+      font-size: 14px;
+      font-weight: 500;
+      padding: 0 0 0 20px;
+    }
+    .chart-wrapper {
+      width: 100%;
+      height: 190px;
+    }
+  }
 }
 </style>
