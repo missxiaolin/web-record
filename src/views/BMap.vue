1658bab2ac2154826d7c6368cda60b36b2c2c9ae
xiaolin <462441355@qq.com>
2020-10-11 19:45:24 +0800

    * 测试

--- src/views/BMap.vue
+++ src/views/BMap.vue
@@ -1,18 +1,162 @@
 <template>
-  <div class="">
-    ss
-  </div>
+  <v-chart :options="options" />
 </template>
 
 <script>
+import "echarts/extension/bmap/bmap";
 
 export default {
-  name: 'B-map',
-  components: {
-  }
-}
+  data() {
+    return {
+      options: {},
+    };
+  },
+  mounted() {
+    this.options = {
+      title: {
+        text: '全国主要城市空气质量 - 百度地图',
+        subtext: 'data from PM25.in',
+        sublink: 'http://www.pm25.in',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      bmap: {
+        key: "G1LFyjrNGIkns5OfpZnrCGAKxpycPLwb",
+        center: [104.114129, 37.550339],
+        zoom: 5,
+        roam: false,
+        mapStyle: {
+          styleJson: [
+            {
+              featureType: "water",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "land",
+              elementType: "all",
+              stylers: {
+                color: "#f3f3f3",
+              },
+            },
+            {
+              featureType: "railway",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "highway",
+              elementType: "all",
+              stylers: {
+                color: "#fdfdfd",
+              },
+            },
+            {
+              featureType: "highway",
+              elementType: "labels",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "arterial",
+              elementType: "geometry",
+              stylers: {
+                color: "#fefefe",
+              },
+            },
+            {
+              featureType: "arterial",
+              elementType: "geometry.fill",
+              stylers: {
+                color: "#fefefe",
+              },
+            },
+            {
+              featureType: "poi",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "green",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "subway",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "manmade",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "local",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "arterial",
+              elementType: "labels",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "boundary",
+              elementType: "all",
+              stylers: {
+                color: "#fefefe",
+              },
+            },
+            {
+              featureType: "building",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "label",
+              elementType: "labels.text.fill",
+              stylers: {
+                color: "#999999",
+              },
+            },
+          ],
+        },
+      },
+      
+    };
+  },
+};
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.b-map {
+  width: 100%;
+  height: 100vh;
+}
+.echarts {
+  width: 100%;
+  height: 100vh;
+}
 </style>
