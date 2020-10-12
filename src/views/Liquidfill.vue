042e36d3aec146a83e8f27d16f2530dde060e8a5
xiaolin <462441355@qq.com>
2020-10-12 22:02:45 +0800

    * 水球图测试

--- src/views/Liquidfill.vue
+++ src/views/Liquidfill.vue
@@ -1,18 +1,35 @@
 <template>
-  <div id="container">
-    s
-  </div>
+  <div id="container"></div>
 </template>
 
 <script>
+import "echarts-liquidfill";
 
 export default {
-  name: 'liquidfill',
-  components: {
-  }
-}
+  mounted() {
+    const data = [0.68, 0.4, 0.2];
+    const chart = this.$echarts.init(document.getElementById("container"));
+    chart.setOption({
+      series: [
+        {
+          type: "liquidFill",
+          data,
+          color: ['red', 'blue', 'yellow'], // 颜色控制
+          itemStyle: {
+              opacity: 0.6, // 透明度
+          },
+          emphasis: {
+              itemStyle: {
+                  opacity: 0.9, // 选中透明度
+              }
+          }
+        },
+      ],
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+#container {width: 100%;height: 100vh;}
 </style>
