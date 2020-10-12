520e4775474d343538ce9e657e894d67fc5aed5c
xiaolin <462441355@qq.com>
2020-10-12 23:15:54 +0800

    * 水球图实现

--- src/components/LiqiudFill/index.vue
+++ src/components/LiqiudFill/index.vue
@@ -1,9 +1,69 @@
 <template>
-    <div></div>
+  <ve-liquidfill :data="chartData" :settings="chartSettings" height="100%" />
 </template>
 
 <script>
-export default {
-    
+function getColor(value) {
+  return value >0 && value <= 0.5 ? 'rgba(97,216,0,.7)'
+    : value > 0.5 && value <= 0.9 ? 'rgba(204, 178, 26,.7)'
+        : value > 0.8 ? 'rgba(241, 47, 28,.7)' : '#c7c7cb ';
 }
+export default {
+  data() {
+    return {
+      chartData: {
+        columns: ["title", "percent"],
+        rows: [
+          {
+            title: "rate",
+            percent: 0.68,
+          },
+        ],
+      },
+      chartSettings: {},
+    };
+  },
+  mounted() {
+    this.chartSettings = {
+      seriesMap: {
+        rate: {
+          radius: "80%",
+          label: {
+            //   normal: {
+            //     formatter: (v) => {
+            //       return `${v.data.value}%`;
+            //     },
+            //     textStyle: {
+            //       fontSize: 36,
+            //       color: "#999",
+            //       fontWeight: "normal",
+            //     },
+            //     position: ["50%", "50%"], // 文字位置
+            //   },
+            insideColor: "#fff",
+          },
+          outline: {
+            itemStyle: {
+              borderColor: "#aaa4a4",
+              borderWidth: 1,
+              color: "none",
+              shadowBlur: 0,
+              shadowColor: "#fff",
+            },
+            borderDistance: 0,
+          },
+          backgroundStyle: {
+            color: "#fff",
+          },
+          itemStyle: {
+            shadowBlur: 0,
+            shadowColor: "#fff",
+          },
+          amplitude: 8,
+          color: [getColor(this.chartData.rows[0].percent)],
+        },
+      },
+    };
+  },
+};
 </script>
