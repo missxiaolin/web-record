597302901ac24abce89e9eeb1cba2ad3dee3b120
xiaolin <462441355@qq.com>
2020-10-13 22:32:10 +0800

    * 测试

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Bmap from '../views/BMap.vue'
 import Bmap2 from '../views/BMap2.vue'
 import Liquidfill from '../views/Liquidfill.vue'
+import WordCloud from '../views/WordCloud.vue'
 
 Vue.use(VueRouter)
 
@@ -24,6 +25,10 @@ const routes = [
     path: '/liqu',
     name: 'liqu',
     component: Liquidfill
+  }, {
+    path: '/word',
+    name: 'word',
+    component: WordCloud
   }
 ]
 
---
+++ src/views/WordCloud.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="container"></div>
+</template>
+
+<script>
+import 'echarts-wordcloud'
+export default {
+    mounted() {
+        const data = [{
+            name: '测试',
+            value: 100
+        }, {
+            name: '数据可视化',
+            value: 50
+        }]
+        const chart = this.$echarts.init(document.getElementById("container"));
+        chart.setOption({
+            series: [{
+                type: 'wordCloud',
+                data
+            }]
+        })
+    }
+}
+</script>
+
+<style lang="scss">
+    #container {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
