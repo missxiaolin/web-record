a8c239967356a8b703d1385957cd830b3e84c3a3
xiaolin <462441355@qq.com>
2020-10-13 22:41:01 +0800

    * 自定义颜色

--- src/components/WordCloud/index.vue
+++ src/components/WordCloud/index.vue
@@ -1,25 +1,29 @@
 <template>
-  <ve-wordcloud :data="chartData" height="100%" />
+  <ve-wordcloud :data="chartData" height="100%" :settings="chartSettings" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-        chartData: {
-            columns: ['name', 'value'],
-            rows: [{
-                name: '一生',
-                value: 100
-            }, {
-                name: '爱你',
-                value: 150
-            }]
-        }
+      chartData: {
+        columns: ["name", "value"],
+        rows: [
+          {
+            name: "一生",
+            value: 100,
+          },
+          {
+            name: "爱你",
+            value: 150,
+          },
+        ],
+      },
+      chartSettings: {
+        color: ["red", "yellow"],
+      },
     };
   },
-  mounted() {
-
-  }
+  mounted() {},
 };
 </script>
