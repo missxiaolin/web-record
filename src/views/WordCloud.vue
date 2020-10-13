5194480f628df86b0b08f96626fbadadb2ea3839
xiaolin <462441355@qq.com>
2020-10-13 22:32:45 +0800

    * 格式化

--- src/views/WordCloud.vue
+++ src/views/WordCloud.vue
@@ -1,32 +1,37 @@
 <template>
-    <div id="container"></div>
+  <div id="container"></div>
 </template>
 
 <script>
-import 'echarts-wordcloud'
+import "echarts-wordcloud";
 export default {
-    mounted() {
-        const data = [{
-            name: '测试',
-            value: 100
-        }, {
-            name: '数据可视化',
-            value: 50
-        }]
-        const chart = this.$echarts.init(document.getElementById("container"));
-        chart.setOption({
-            series: [{
-                type: 'wordCloud',
-                data
-            }]
-        })
-    }
-}
+  mounted() {
+    const data = [
+      {
+        name: "测试",
+        value: 100,
+      },
+      {
+        name: "数据可视化",
+        value: 50,
+      },
+    ];
+    const chart = this.$echarts.init(document.getElementById("container"));
+    chart.setOption({
+      series: [
+        {
+          type: "wordCloud",
+          data,
+        },
+      ],
+    });
+  },
+};
 </script>
 
 <style lang="scss">
-    #container {
-        width: 100%;
-        height: 100vh;
-    }
+#container {
+  width: 100%;
+  height: 100vh;
+}
 </style>
