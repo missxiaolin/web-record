893d87dffd2248a6ba36778a844fc06b367d701c
xiaolin <462441355@qq.com>
2020-11-04 22:40:29 +0800

    * add

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import datav from 'datav-libs'
+import datav from 'datav-libs/src/components/Test/index'
 
 createApp(App).use(store).use(datav).use(router).mount('#app')
