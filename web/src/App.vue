65cd2703d760121576950a145d1ebe591b19a074
xiaolin <462441355@qq.com>
2020-11-07 20:20:31 +0800

    * add

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div id="nav">
+  <div id="icon-wrapper">
     <svg-component></svg-component>
+    <icon-component name="close" class="icon"></icon-component>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.icon {
+  width: 50px;
+  height: 50px;
 }
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import datav from 'datav-libs/src/components/Svg/index.js'
+import datav from 'datav-libs'
+import Svg from 'datav-libs/src/components/Svg/index'
 
-createApp(App).use(store).use(datav).use(router).mount('#app')
+createApp(App).use(store).use(datav).use(Svg).use(router).mount('#app')
