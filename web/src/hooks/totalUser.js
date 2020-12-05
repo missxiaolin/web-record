cfac4add051cfab590a55561c1e9954961a1075e
xiaolin <462441355@qq.com>
2020-12-05 11:54:02 +0800

    * 引入测试

--- web/src/hooks/totalUser.js
+++ web/src/hooks/totalUser.js
@@ -8,7 +8,7 @@ export default function () {
 
     onMounted(() => {
         task = setInterval(() => {
-            todayUser.value = todayUser.value + 1;
+            todayUser.value = todayUser.value + 10;
             growthLastDay.value = growthLastDay.value + 1
             growthLastMonth.value = growthLastMonth.value + 1
         }, 1000);
