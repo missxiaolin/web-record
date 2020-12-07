8e1ce1ea447afdbfd57b561a3393d1a9f75cc467
xiaolin <462441355@qq.com>
2020-12-07 21:25:40 +0800

    * moke 数据

--- web/src/hooks/totalUser.js
+++ web/src/hooks/totalUser.js
@@ -24,6 +24,7 @@ const ageMockData = [
 ]
 const deviceMockData = { "totalDevices": 1070909, "devices": [{ "key": "Android", "value": 423676 }, { "key": "iOS", "value": 373581 }, { "key": "PC", "value": 273652 }] }
 const genderMockData = [{ "key": "male", "value": 1442542 }, { "key": "female", "value": 1442548 }]
+const riderMockData = { "axisX": ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"], "orderData": { "legend1": "去年骑手月人均接单", "legend2": "今年骑手月人均接单", "data1": ["330", "420", "560", "450", "610", "890", "720", "610", "580", "750", "770", "600"], "data2": ["430", "510", "660", "550", "710", "990", "620", "550", "760", "810", "930", "720"] }, "rateData": { "legend1": "去年月新增骑手", "legend2": "今年月新增骑手", "data1": ["129", "223", "202", "197", "300", "112", "333", "249", "178", "322", "401", "167"], "data2": ["179", "263", "282", "297", "330", "344", "222", "299", "190", "455", "566", "233"] } }
 
 export default function () {
     let todayUser = ref(1000),
@@ -33,6 +34,7 @@ export default function () {
         ageData = ref(ageMockData),
         deviceData = ref(deviceMockData),
         genderData = ref(genderMockData),
+        riderData = ref(riderData),
         task = '';
 
     onMounted(() => {
@@ -66,6 +68,7 @@ export default function () {
         averageAge,
         ageData,
         deviceData,
-        genderData
+        genderData,
+        riderData
     }
 }
