b8cee75d2339719ff9dc6ed720403b646faa8176
xiaolin <462441355@qq.com>
2020-12-29 09:02:52 +0800

    * 测试

--- web/src/views/Test.vue
+++ web/src/views/Test.vue
@@ -1,23 +1,42 @@
 <template>
-  <div>
-      <base-scroll-list></base-scroll-list>
+  <div class="box">
+    <base-scroll-list :config="config"></base-scroll-list>
   </div>
 </template>
 <script>
-import useTest from './useTest'
+import useTest from "./useTest";
+import { ref } from "vue";
 
 export default {
-    setup() {
-        return {
-            
-        }
-    }
-}
+  setup() {
+    const header = ref([]);
+    const headerStyle = ref([]);
+    const headerHeight = ref(40);
+    const config = ref({});
+    config.value = {
+      headerData: ["姓名", "年龄", "月薪"],
+      headerStyle: [{ color: "red" }],
+      headerHeight: 40,
+      headerIndex: true
+    };
+
+    return {
+      header,
+      headerStyle,
+      headerHeight,
+      config,
+    };
+  },
+};
 </script>
 
 <style>
-    #app {
-        align-items:flex-start;
-        justify-content: flex-start;
-    }
+#app {
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+.box {
+  width: 500px;
+  height: 500px;
+}
 </style>
